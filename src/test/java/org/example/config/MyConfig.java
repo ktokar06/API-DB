2bfc1ecfe3fc7ae0f77193a8d19821fc28df34db
K <ktokar06@mail.ru>
2025-03-05 20:09:38 +0300

    Update Commit

--- src/test/java/org/example/config/MyConfig.java
+++ src/test/java/org/example/config/MyConfig.java
@@ -10,4 +10,11 @@ public class MyConfig {
     public static final String URL_USERS_ENDPOINT = "/index.php?rest_route=/wp/v2/users";
     public static final String USERNAME = "Firstname.LastName";
     public static final String PASSWORD = "123-Test";
+    public static final String TEST_TITLE = "Test Title";
+    public static final String TEST_CONTENT = "Test Content";
+    public static final String UPDATED_TITLE = "Updated Title";
+    public static final String UPDATED_CONTENT = "Updated Content";
+    public static final String POST_STATUS = "publish";
+    public static final int AUTHOR_ID = 1;
+    public static final int NON_EXISTENT_ID = 999;
 }
