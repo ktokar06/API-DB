45122bbba7eff08d57e878f5745a2bcd60c4be37
K <103377916+ktokar06@users.noreply.github.com>
2025-05-28 05:33:21 +0300

    Update MyConfig.java

--- src/test/java/org/example/config/MyConfig.java
+++ src/test/java/org/example/config/MyConfig.java
@@ -5,10 +5,10 @@ package org.example.config;
  * Класс для хранения конфигурационных констант приложения.
  * */
 public class MyConfig {
-    public static final String URL = "http://localhost:8000";
-    public static final String URL_POSTS_ENDPOINT = "/index.php?rest_route=/wp/v2/posts";
-    public static final String URL_TAGS_ENDPOINT = "/index.php?rest_route=/wp/v2/tags";
-    public static final String URL_USERS_ENDPOINT = "/index.php?rest_route=/wp/v2/users";
-    public static final String USERNAME = "Firstname.LastName";
-    public static final String PASSWORD = "123-Test";
+    public static final String URL = "url_site";
+    public static final String URL_POSTS_ENDPOINT = "url_site";
+    public static final String URL_TAGS_ENDPOINT = "url_site";
+    public static final String URL_USERS_ENDPOINT = "url_site";
+    public static final String USERNAME = "name";
+    public static final String PASSWORD = "password";;
 }
